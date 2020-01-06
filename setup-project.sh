e68ce50dcedf91a1db888d3ee64a2da9c86e78f2
Xett <3954937+Xett@users.noreply.github.com>
2020-01-06 23:35:39 +1000

    Automate project variables

--- setup-project.sh
+++ setup-project.sh
@@ -1,10 +1,10 @@
 # Read Input Variables
-read -p "Enter Project Name: " setupName
-read -p "Enter Project Version Number: " setupVersion
-read -p "Enter Author Name: " setupAuthor
-read -p "Enter Author Email Address: " setupEmail
-read -p "Enter Short Description: " setupDescription
-read -p "Enter Project URL: " setupUrl
+setupName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
+setupVersion="0.1"
+setupAuthor=$(git config user.name)
+setupEmail=$(git config user.name)
+setupUrl=$(git remote get-url origin)
+setupDescription=$(python get-project-short-description.py $setupUrl)
 
 # Set Directory Variables
 dir=$(PWD) > /dev/null 2>&1
@@ -27,3 +27,8 @@ printf "import setuptools\nwith open(\"README.md\",\"r\") as fh:\n\tlong_descrip
 
 # Create bin script
 printf "#!/usr/bin/env python\nimport $setupName" > $bin_dir"/"$setupName
+
+# Clean the directory of this project
+rm $dir"/setup-project.sh"
+rm $dir"/get-project-short-description.py"
+printf "" > .gitignore
