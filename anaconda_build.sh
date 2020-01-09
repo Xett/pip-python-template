a690ba544b77ad3e49c256f23cd8e70564741eb0
Xett <3954937+Xett@users.noreply.github.com>
2020-01-10 00:29:15 +1000

    Fix anaconda build typo

--- anaconda_build.sh
+++ anaconda_build.sh
@@ -1,3 +1,3 @@
 setupName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
-conda_build "./anaconda_build/$setupName"
+conda build "./anaconda_build/$setupName"
 conda install --use-local $setupName
